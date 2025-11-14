49d1ca56f45b587302ef4d3ef4963fd7098e167f
Ujjwal-rajak <ujjwalrajak2002@gmail.com>
2025-11-14 19:19:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,6 +17,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin:"https://veritas-learning-1.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials:true
 }))
 app.use("/api/auth", authRouter)
@@ -35,5 +36,6 @@ app.listen(port , ()=>{
     console.log("Server Started")
     connectDb()
 })
+
 
 
